7f6a2fba42c92428da8037860f7561f6f0d62610
Gabriel <Gabriel@Gabriel-PC>
2016-07-10 14:27:21 -0300

    Segundo projeto

---
+++ Teste/src/Teste/Carro.java
@@ -0,0 +1,11 @@
+package Teste;
+public class Carro {
+	String nome;
+	int preco;
+
+	public Carro(String nome, int preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+}
---
+++ Teste/src/Teste/Teste.java
@@ -0,0 +1,10 @@
+package Teste;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		Carro c = new Carro("Gol", 40000);
+		System.out.println("Nome: "+c.nome+" Preco: "+c.preco);
+	}
+
+}
